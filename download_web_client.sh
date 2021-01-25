0d89d24bc3e76f59c9aea6cc5eebcd6176b58bad
Óscar de Arriba <odarriba@hey.com>
2021-01-25 14:04:52 +0100

    Update web client

--- download_web_client.sh
+++ download_web_client.sh
@@ -9,7 +9,7 @@ set -e
 rm -rf ./tmp/web
 git clone https://github.com/bitwarden/web.git ./tmp/web
 cd ./tmp/web
-git checkout tags/v2.5.0
+git checkout tags/v2.18.0
 git submodule init
 git submodule update
 npm install && npm run dist
