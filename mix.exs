ee5c3833c17fdd3fc061dac1c3c318fd299188a7
Óscar de Arriba <odarriba@hey.com>
2021-08-25 10:36:10 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bitwardex.MixProject do
   def project do
     [
       app: :bitwardex,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
